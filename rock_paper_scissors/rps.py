cd97d4659bc98a3ccebbc5d6fed51678594f9cbe
Mark King <markpkng@gmail.com>
2020-02-12 00:20:31 -0500

    rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,7 +4,6 @@ import sys
 
 
 def rock_paper_scissors(n):
-    rps_array = [['rock'], ['paper'], ['scissors']]
     if n == 0:
         return [[]]
 
@@ -14,12 +13,12 @@ def rock_paper_scissors(n):
         else:
             new_arr = []
             for i in range(len(arr)):
-                new_arr.append(arr[i] + rps_array[0])
-                new_arr.append(arr[i] + rps_array[1])
-                new_arr.append(arr[i] + rps_array[2])
+                new_arr.append(arr[i] + ['rock'])
+                new_arr.append(arr[i] + ['paper'])
+                new_arr.append(arr[i] + ['scissors'])
 
             return(rps_helper(new_arr, iter + 1))
-    return rps_helper(rps_array, 1)
+    return rps_helper([['rock'], ['paper'], ['scissors']], 1)
 
 
 if __name__ == "__main__":
