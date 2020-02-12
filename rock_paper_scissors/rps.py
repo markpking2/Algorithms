83b4be97fa2f09fcaaee25bd64fd8af867b42c62
Mark King <markpkng@gmail.com>
2020-02-12 00:14:19 -0500

    rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,29 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    rps_array = [['rock'], ['paper'], ['scissors']]
+    if n == 0:
+        return [[]]
+
+    def rps_helper(arr):
+        if(len(arr[0]) >= n):
+            return arr
+        else:
+            new_arr = []
+            for i in range(len(arr)):
+                new_arr.append(arr[i] + rps_array[0])
+                new_arr.append(arr[i] + rps_array[1])
+                new_arr.append(arr[i] + rps_array[2])
+
+            return(rps_helper(new_arr))
+    return rps_helper(rps_array)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
